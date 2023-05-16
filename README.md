0300c38652eaf8a972a4bbbb42570109d6c2abf6
Thomas Rousseau <thomas.rousseau@proton.me>
2023-05-16 16:59:53 +0200

    Création du script
    typo

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# calcul_cout_categorie
+# Script de calcul des coûts par catégorie
+
+Ce script permet de calculer le coût total pour chaque catégorie de termes de recherche dans un fichier CSV donné.
+
+## Prérequis
+
+Avant d'utiliser ce script, assurez-vous d'avoir les éléments suivants installés :
+
+- Python (version 3.11)
+- pip (outil de gestion des packages Python)
+
+## Installation
+
+1. Clonez ou téléchargez ce dépôt sur votre machine.
+
+2. Ouvrez un terminal ou une invite de commandes et naviguez jusqu'au répertoire du script.
+
+3. Installez les dépendances en exécutant la commande suivante :
+`pip install -r requirements.txt`
+
+Cela installera toutes les bibliothèques requises pour exécuter le script.
+
+## Utilisation
+
+1. Placez votre fichier CSV contenant les données dans le répertoire `Desktop/test` du projet et renommez le en `data.csv`.
+
+2. Renommez le fichier `termes.json.exemple` en `termes.json`
+
+3. Renseignez les termes de recherche en respectant la structure du fichier.
+
+4. Exécutez le script en exécutant la commande suivante dans le terminal :
+`python script.py` ou `py script.py` ou autre suivant votre installation
+
+
+5. Les résultats seront affichés dans la console, présentant le coût total pour chaque catégorie de termes de recherche.
+
+6. Les résultats seront affichés dans la console, montrant le coût total de chaque catégorie de termes de recherche.
+
+## Exemple de structure de fichier CSV
+
+Assurez-vous que votre fichier CSV suit la structure suivante :
+```
+Terme de recherche,Coût
+Recherche 1,10.50
+Recherche 2,15.75
+Recherche 3,5.25
+...
+```
+
+- La première ligne du fichier CSV doit contenir les en-têtes des colonnes (Terme de recherche, Coût).
+- Les termes de recherche et les coûts correspondants doivent être placés dans les colonnes appropriées.
+- Le fichier peut contenir d'autres colonnes, qui seront ignorées
---
+++ script.py
@@ -0,0 +1,93 @@
+import csv
+import os
+import json
+from tabulate import tabulate
+from termcolor import colored
+
+# Configuration
+file_name = 'data.csv'
+
+current_user_path = os.getcwd()
+
+# Obtention du chemin absolu du fichier json dans le dossier courant
+json_file_path = os.path.join(current_user_path, 'termes.json')
+
+# Obtention du chemin absolu du fichier CSV dans le dossier courant
+csv_file_path = os.path.join(current_user_path, file_name)
+
+# Lecture du fichier JSON et stockage des termes de recherche dans specific_terms
+with open(json_file_path, 'r') as json_file:
+    specific_terms = json.load(json_file)
+
+# Affichage des informations
+print(colored("###################################################", "cyan"))
+print(colored(f"Termes de recherches : {specific_terms}", "yellow"))
+print(colored("###################################################", "cyan"))
+
+# Vérification de l'existence du fichier CSV
+if not os.path.exists(csv_file_path):
+    print(colored(f"Erreur : Le fichier CSV '{file_name}' n'a pas été trouvé dans le dossier '{directory_path}'.", "red"))
+    exit(1)
+
+# Dictionnaire pour stocker les totaux
+totals = {'divers': 0}
+
+try:
+    # Ouverture du fichier CSV
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        header = next(reader)
+
+        # Vérification des en-têtes requis
+        if 'Terme de recherche' not in header or 'Coût' not in header:
+            print(colored("Erreur : Les en-têtes 'Terme de recherche' et 'Coût' sont requis dans le fichier CSV.", "red"))
+            exit(1)
+
+        # Récupération des index des colonnes
+        term_index = header.index('Terme de recherche')
+        cost_index = header.index('Coût')
+
+        # Lecture du fichier CSV
+        for row in reader:
+            # Vérification du nombre de colonnes
+            if len(row) != len(header):
+                print(colored(f"Attention : La ligne {reader.line_num} ne contient pas le bon nombre de colonnes.", "yellow"))
+                continue
+
+            term = row[term_index]
+            cost_str = row[cost_index].replace(',', '.')
+
+            # Vérification de la validité du coût
+            try:
+                cost = float(cost_str)
+            except ValueError:
+                print(colored(f"Attention : La valeur du coût '{cost_str}' à la ligne {reader.line_num} n'est pas valide.", "yellow"))
+                continue
+
+            found_specific_term = False
+            for word in specific_terms:
+                if word.lower() in term.lower():
+                    found_specific_term = True
+                    if word not in totals:
+                        totals[word] = 0
+                    totals[word] += cost
+                    break
+
+            if not found_specific_term:
+                totals['divers'] += cost
+
+    # Préparation du tableau des totaux
+    table = []
+    for category, total_cost in totals.items():
+        total_cost = round(total_cost, 2)
+        table.append([category, f"{total_cost} €"])
+
+    # Affichage du tableau
+    print(tabulate(table, headers=[colored("Catégorie", "cyan"), colored("Coût Total (€)", "cyan")], tablefmt="fancy_grid"))
+
+except FileNotFoundError:
+    print(colored(f"Erreur : Le fichier CSV '{file_name}' n'a pas été trouvé.", "red"))
+    exit(1)
+except csv.Error as e:
+    print(colored(f"Erreur lors de la lecture du fichier CSV : {e}", "red"))
+    exit(1)
